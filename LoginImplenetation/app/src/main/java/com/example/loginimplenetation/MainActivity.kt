6196317b6b978fda302d2f4c3862837eee189e60
Andrew Gregersen <andrew.gregersen@bellevuecollege.edu>
2020-10-24 19:38:59 -0700

    Finished Functional Login Design
    Formatted Login screen and started implementing the functions needed to implement it

--- LoginImplenetation/app/src/main/java/com/example/loginimplenetation/MainActivity.kt
+++ LoginImplenetation/app/src/main/java/com/example/loginimplenetation/MainActivity.kt
@@ -2,10 +2,55 @@ package com.example.loginimplenetation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        googleLogin.setOnClickListener{
+
+        }
+
+        Login.setOnClickListener {
+            if(checkRegister(Username.text.toString(), Password.text.toString())) //if login is successful
+                setContentView(R.layout.main_fragment) //load user profile
+
+        }
+
+
     }
+
+    class MainFragment : Fragment(){
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle?): View {
+            return inflater.inflate(R.layout.main_fragment, container, false)
+        }
+    }
+
+
+
+
+
+
+
+
+   private fun checkRegister(username: String, password: String): Boolean{
+       /*
+       Ask nezi to implemnt this part since he is building the DB and will know more about it.
+        */
+
+       return if(username != null)
+           password !=null;
+       else false;
+
+   }
+
+
+
 }
