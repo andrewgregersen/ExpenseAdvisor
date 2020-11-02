adb16b4d7b5690053dea780f80b9323c32d1ef58
Andrew Gregersen <andrew.gregersen@bellevuecollege.edu>
2020-11-01 19:09:42 -0800

    Further implemented code
    just more work attempting to get the app to be able to log in with Google

--- LoginImplenetation/app/src/main/java/com/example/loginimplenetation/MainActivity.kt
+++ LoginImplenetation/app/src/main/java/com/example/loginimplenetation/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.Switch
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
@@ -13,57 +14,106 @@ import com.google.android.gms.auth.api.signin.*
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.Exception
 
 
-class MainActivity: AppCompatActivity(){
-    private lateinit var auth : FirebaseAuth
+class MainActivity: AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
 
-    public override fun onStart(){
+    public override fun onStart() {
         super.onStart()
         //checks to see if there is an already signed in account
         var account = GoogleSignIn.getLastSignedInAccount(this)
         val currentUser = auth.currentUser
-        if(currentUser == null){
-            if(account==null){
-                if(faccount == null){
+        if (currentUser == null) {
+            if (account == null) {
+                if (faccount == null) {
                     updateUI(null)
                 }
-            }else updateUI(account)
-        }else updateUI(currentUser)
+            } else updateUI(account)
+        } else updateUI(currentUser)
+
+
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        auth = Firebase.auth
+        val googleButton = findViewById<Button>(R.id.google_Login).setOnClickListener(google_Login)
+        //val facebookButton = findViewById<Button>(R.id.facebook_Login).setOnClickListener(facebook_Login)
+        val loginButton = findViewById<Button>(R.id.Login).setOnClickListener()
+
+
 
 
 
     }
 
 
-    private fun googleSignIn(){
-         val signInIntent = mGoogleSignInClient.signInIntent
-        startActrivityForResult(signInIntent, RC_SIGN_IN)
+    override fun onClick(v:View) {
+        when (v.id) {
+            R.id.Login -> signIn(R.id.Username.toString(),R.id.Password.toString())
+            R.id.google_Login -> signIn()
+            //R.id.facebook_Login -> signIn() need to implement
+            R.id.signout -> signOut() //need to implement
+
+
+        }
     }
 
-    override fun onActivityResult(requestCode: Int,resultCode: Int, data: Intent?){
+    private fun signIn(){
+        val intent = mGoogleSignInClient.signInIntent
+        startActivity(intent, RC_SIGN_IN)
+    }
+
+
+
+
+
+
+
+
+
+
+
+override fun onActivityResult(requestCode: Int,resultCode: Int, data: Intent?){
         super.onActivityResult(requestCode,resultCode,data)
 
         if(requestCode == RC_SIGN_IN){
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
             try{
                 val account = task.getResult(ApiException::class.java)!!
-                Log.d(TAG, "firebaseAuthWithGoogle:"+ account.id)
+                Log.d(TAG2, "firebaseAuthWithGoogle:"+ account.id)
                 firebaseAuthWithGoogle(account.idToken!!)
             }catch (e: Exception){
-                Log.w(TAG, "Google sign in failed" ,e)
+                Log.w(TAG2, "Google sign in failed" ,e)
+                updateUI(null)
             }
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?){
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build()
-        var mGoogleSignInClient = GoogleSignIn.getClient(this,gso)
-        findViewById(R.id.google_Login).setOnClickListener(this)
-
+    private fun firebaseAuthWithGoogle(token: String ){
+        val credential = GoogleAuthProvider.getCredential(token, null)
+        auth.signInWithCredential(credential).addOnCompleteListener(this){ task ->
+            if(task.isSuccessful){
+                Log.d(TAG2, "signInWithCredential:success")
+                val user = auth.currentUser
+                updateUI(user)
+            }else{
+                Log.w(TAG2, "signInWithCredential:failure",task.exception)
+                val view = binding.mainLayout
+                Snackbar.make(view,"Authentication Failed.", Snackbar.LENGTH_SHORT).show()
+                updateUI(null)
+            }
+        }
     }
 
     private fun createSignInIntent(){
@@ -71,6 +121,14 @@ class MainActivity: AppCompatActivity(){
             AuthUI.IdpConfig.EmailBuilder().build(),
             AuthUI.IdpConfig.GoogleBuilder().build()
         )
+    }
+
+    private fun signOut(){
+        auth.signOut()
+
+        mGoogleSignInClient.revokeAccess().addOnCompleteListener(this){
+            updateUI(null)
+        }
     }
 
     private fun createAccount(email: String, password: String){
@@ -108,21 +166,27 @@ class MainActivity: AppCompatActivity(){
 
     private fun updateUI(user: FirebaseUser?){
         if(user != null){
-            LoggedInActivity()
-        }
+            binding.status.text = getString(R.string.google_status_fmt, user.email)
+            binding.detail.text = getString(R.string.firebase_status_fmt,user.uid)
 
-    }
-    private fun updateUI(user: GoogleSignInAccount){
-        if(user != null){
 
+            binding.google_Login.visibility = View.VISIBLE
+            binding.signOutAndDisconect.visibility = View.GONE
+        }else{
+            binding.status.setText(R.string.signed_out)
+            binding.detail.text = null
+            binding.google_Login.visibility = View.VISIBLE
+            binding.signOutAndDisconnect.visibility = View.GONE
         }
 
     }
 
 
     companion object {
-        private const val TAG = "EmailPassword"
+        private const val TAG1 = "EmailPassword"
+        private const val TAG2 = "GoogleActivity"
         private const val RC_MULTI_FACTOR = 9005
+        private const val RC_SIGN_IN = ""
     }
 
 
