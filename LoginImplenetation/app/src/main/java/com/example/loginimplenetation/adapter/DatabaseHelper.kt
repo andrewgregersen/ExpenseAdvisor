236608975d0629a6262244c3cbfe7432d0e29f13
arielmukadi <45185718+arielmukadi@users.noreply.github.com>
2021-02-21 22:31:38 -0800

    Updated database
    reciept table and method implemented for retrieval according to the date and the total price, User table droped

--- LoginImplenetation/app/src/main/java/com/example/loginimplenetation/adapter/DatabaseHelper.kt
+++ LoginImplenetation/app/src/main/java/com/example/loginimplenetation/adapter/DatabaseHelper.kt
@@ -7,6 +7,9 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteException
 import android.database.sqlite.SQLiteOpenHelper
 import android.widget.Toast
+import java.sql.Time
+import java.sql.Types.TIMESTAMP
+import javax.xml.datatype.DatatypeConstants.DATETIME
 
 class DatabaseHelper(var Context: Context):
     SQLiteOpenHelper(Context, DATABASE_NAME, null, DATABASE_VERSION) {
@@ -24,9 +27,10 @@ class DatabaseHelper(var Context: Context):
      //Table Receipt
      private const val RECEIPT = "RECEIPT"
      private const val COLUMN_RECEIPT_ID = "RECEIPT_ID"
-     private const val COLUMN_RECEIPT_USER_ID = "UID"
+     //private const val COLUMN_RECEIPT_USER_ID = "UID"
      private const val COLUMN_TOTAL = "TOTAL"
      private const val COLUMN_STORE = "STORE"
+     private const val COLUMN_DATE = "DATE"
 
      //Table Item
      private const val ITEM = "ITEM"
@@ -54,7 +58,7 @@ class DatabaseHelper(var Context: Context):
 
      //Table Purchase
      private const val PURCHASE = "PURCHASE"
-     private const val COLUMN_DATE = "DATE"
+     //private const val COLUMN_DATE = "DATE"
      private const val COLUMN_PURCHASE_USER_ID = "PURCHASE_UID"
      private const val COLUMN_PURCHASE_ITEM_ID = "PURCHASE_ITEM_ID"
 
@@ -131,6 +135,13 @@ class DatabaseHelper(var Context: Context):
                 "REFERENCES "+ CATEGORY + "("+ COLUMN_CATEGORY_ID + ") ON DELETE CASCADE)"
         db?.execSQL(createTableBelong)
 
+        val createTableReceipt= "CREATE TABLE "+ RECEIPT + " (" + COLUMN_RECEIPT_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "+
+               COLUMN_TOTAL + " INTEGER, "+ COLUMN_STORE + " VARCHAR(256), "+ COLUMN_DATE +
+               " DATETIME DEFAULT CURRENT_TIMESTAMP) "
+        db?.execSQL(createTableReceipt)
+
+
+
 
 //        val createTable= "CREATE TABLE " + TABLE_NAME +" (" +
 //                COL_ID +" INTEGER PRIMARY KEY AUTOINCREMENT,"+
@@ -176,33 +187,32 @@ class DatabaseHelper(var Context: Context):
         else
             Toast.makeText(Context, "Success to insert item, Step 1", Toast.LENGTH_LONG).show()
 
-
         /* STEP 2, GET ITEM ID */
         var id= getLastItemID()
 
 
         /**** STEP 3 LINK ITEM AND IT'S CATEGORY ****/
+        val db3 = this.writableDatabase
+        val cv3= ContentValues()
+        cv3.put(COLUMN_BELONG_ITEM_ID, id)
+        cv3.put(COLUMN_BELONG_CATEGORY_ID, getCategoryID(category))
 
-            val db3 = this.writableDatabase
-            val cv3= ContentValues()
-            cv3.put(COLUMN_BELONG_ITEM_ID, id)
-            cv3.put(COLUMN_BELONG_CATEGORY_ID, getCategoryID(category))
+        if(getCategoryID(category) == -1){
+            Toast.makeText(Context, "Failed!! CATEGORY ID doesn't exist: STEP 3", Toast.LENGTH_LONG).show()
+            return
+        }
 
-            if(getCategoryID(category) == -1){
-                Toast.makeText(Context, "Failed!! CATEGORY ID doesn't exist: STEP 3", Toast.LENGTH_LONG).show()
-                return
-            }
+        val solution= db3.insert(BELONG, null, cv3)
 
-            val solution= db3.insert(BELONG, null, cv3)
-
-            if (solution == -1.toLong())
-                Toast.makeText(Context, "Failed to insert in STEP 3", Toast.LENGTH_LONG).show()
-            else
-                Toast.makeText(Context, "Item insertion success", Toast.LENGTH_LONG).show()
+        if (solution == -1.toLong())
+            Toast.makeText(Context, "Failed to insert in STEP 3", Toast.LENGTH_LONG).show()
+        else
+            Toast.makeText(Context, "Item insertion success", Toast.LENGTH_LONG).show()
 
 
-         Toast.makeText(Context, "TOUT VAS BIEN", Toast.LENGTH_LONG).show()
-         db.close()
+        Toast.makeText(Context, "TOUT VAS BIEN", Toast.LENGTH_LONG).show()
+
+        db.close()
     }
 
 
@@ -259,6 +269,7 @@ class DatabaseHelper(var Context: Context):
         cv.put(COLUMN_BELONG_ITEM_ID, 1)
 
 
+
         val result = db.insert(BELONG, null, cv)
 
         if (result == -1.toLong())
@@ -266,6 +277,37 @@ class DatabaseHelper(var Context: Context):
         else
             Toast.makeText(Context, "Success", Toast.LENGTH_LONG).show()
     }
+
+    fun insertReceipt(total: Int, store: String){
+        val db = this.writableDatabase
+        val cv = ContentValues()
+        cv.put(COLUMN_TOTAL, total)
+        cv.put(COLUMN_STORE, store)
+
+        val result = db.insert(RECEIPT, null, cv)
+
+
+
+        if (result == -1.toLong())
+            Toast.makeText(Context, result.toString(), Toast.LENGTH_LONG).show()
+        else
+            Toast.makeText(Context, "Success" + total.toString() + " "+ store, Toast.LENGTH_LONG).show()
+
+    }
+
+    fun insertContains(receiptID: Int, itemID: Int){
+        val db = this.writableDatabase
+        val cv = ContentValues()
+        cv.put(COLUMN_CONTAINS_ITEM_ID, itemID)
+        cv.put(COLUMN_CONTAINS_RECEIPT_ID, receiptID)
+
+        val result = db.insert(CONTAINS, null, cv)
+
+
+
+    }
+
+
 
 
 
@@ -325,6 +367,25 @@ class DatabaseHelper(var Context: Context):
     }
 
 
+    fun getCategoryName(categoryID: Int): String{
+
+        val db = this.readableDatabase
+        val query= "SELECT " + COLUMN_CATEGORY_NAME + " FROM " + CATEGORY + " WHERE " + COLUMN_CATEGORY_ID + " = " +
+                categoryID + " ORDER BY " + COLUMN_CATEGORY_NAME + " DESC LIMIT 1"
+
+        var cursor = db.rawQuery(query, null)
+
+        if(cursor.moveToNext()){
+            // Toast.makeText(Context, cursor.getString(cursor.getColumnIndex(COLUMN_ITEM_ID)), Toast.LENGTH_LONG).show()
+        }
+        var category = cursor.getString(cursor.getColumnIndex(COLUMN_CATEGORY_NAME))
+        cursor.close()
+
+        return category
+
+    }
+
+
     fun isCategory(category: String): Boolean{
         val db = this.readableDatabase
         val query = "SELECT " + COLUMN_CATEGORY_ID + " FROM " + CATEGORY + " WHERE " +
@@ -341,6 +402,24 @@ class DatabaseHelper(var Context: Context):
 
     }
 
+
+
+    fun getItemCategoryInBelong(id: Int): Int{
+
+        val db = this.readableDatabase
+        val query = "SELECT " + COLUMN_BELONG_CATEGORY_ID + " FROM " + BELONG + " WHERE " +
+                COLUMN_BELONG_ITEM_ID + " = "+ id + " ORDER BY " + COLUMN_BELONG_CATEGORY_ID + " LIMIT 1"
+
+        var cursor = db.rawQuery(query, null)
+
+        if(!cursor.moveToNext()){
+            // Toast.makeText(Context, "category "+ category + " doesn't exist", Toast.LENGTH_LONG).show()
+        }
+
+        var id = cursor.getString(cursor.getColumnIndex(COLUMN_BELONG_CATEGORY_ID)).toInt()
+        Toast.makeText(Context,"category id in belong is " + id, Toast.LENGTH_LONG).show()
+        return id
+    }
 
     fun getItemsOfCategory(category: String): MutableList<String> {
 
@@ -382,6 +461,23 @@ class DatabaseHelper(var Context: Context):
         Toast.makeText(Context, "" + list.size+ " item(s) found ", Toast.LENGTH_LONG).show()
         return list
 
+    }
+
+    fun getPriceOfSingleItem(id: Int):Int{
+
+        val db = this.readableDatabase
+        val query= "SELECT " + COLUMN_PRICE + " FROM " + ITEM + " WHERE " + COLUMN_ITEM_ID + " = " +
+                id + " ORDER BY " + COLUMN_PRICE + " DESC LIMIT 1"
+
+        var cursor = db.rawQuery(query, null)
+
+        if(cursor.moveToNext()){
+            // Toast.makeText(Context, cursor.getString(cursor.getColumnIndex(COLUMN_ITEM_ID)), Toast.LENGTH_LONG).show()
+        }
+        var price = cursor.getString(cursor.getColumnIndex(COLUMN_PRICE)).toInt()
+        cursor.close()
+
+        return price
     }
 
     fun getOnlyPriceOfItemOfCategory(category: String):MutableList<String>{
@@ -429,6 +525,7 @@ class DatabaseHelper(var Context: Context):
            // Toast.makeText(Context, cursor.getString(cursor.getColumnIndex(COLUMN_ITEM_ID)), Toast.LENGTH_LONG).show()
         }
         var id = cursor.getString(cursor.getColumnIndex(COLUMN_ITEM_ID)).toInt()
+        cursor.close()
         return id
     }
 
@@ -446,6 +543,155 @@ class DatabaseHelper(var Context: Context):
         var id = cursor.getString(cursor.getColumnIndex(COLUMN_ITEM_ID)).toInt()
         return id
 
+    }
+
+
+    fun getAll_Receipt_ID(): MutableList<Int>{
+
+        var list: MutableList<Int> = ArrayList()
+        val db = this.readableDatabase
+        val query= "SELECT " + COLUMN_RECEIPT_ID + " FROM " + RECEIPT + " ORDER BY " + COLUMN_RECEIPT_ID + " DESC "
+        var cursor: Cursor? = null
+        try {
+            cursor = db.rawQuery(query, null)
+        } catch (e: SQLiteException){
+            db.execSQL(query)
+            return ArrayList()
+        }
+
+        var id :Int
+
+        if(cursor.moveToFirst()){
+            do {
+
+                id= cursor.getString(cursor.getColumnIndex(COLUMN_RECEIPT_ID)).toInt()
+                list.add(id)
+                //Toast.makeText(Context, name, Toast.LENGTH_LONG).show()
+
+            }while (cursor.moveToNext())
+        }
+
+        return list
+    }
+
+    fun getAll_Total_Receipt_ID(): MutableList<String>{
+
+        var list: MutableList<String> = ArrayList()
+        val db = this.readableDatabase
+        val query= "SELECT " + COLUMN_TOTAL + " FROM " + RECEIPT
+        var cursor: Cursor? = null
+        try {
+            cursor = db.rawQuery(query, null)
+        } catch (e: SQLiteException){
+            db.execSQL(query)
+            return ArrayList()
+        }
+
+        var price :String
+
+        if(cursor.moveToFirst()){
+            do {
+
+                price = cursor.getString(cursor.getColumnIndex(COLUMN_TOTAL)).toString()
+                list.add(price)
+                //Toast.makeText(Context, name, Toast.LENGTH_LONG).show()
+
+            }while (cursor.moveToNext())
+        }
+
+        return list
+    }
+
+
+    fun getAll_Date_Receipt_ID(): MutableList<String>{
+
+        var list: MutableList<String> = ArrayList()
+        val db = this.readableDatabase
+        val query= "SELECT " + COLUMN_DATE + " FROM " + RECEIPT
+        var cursor: Cursor? = null
+        try {
+            cursor = db.rawQuery(query, null)
+        } catch (e: SQLiteException){
+            db.execSQL(query)
+            return ArrayList()
+        }
+
+        var price :String
+
+        if(cursor.moveToFirst()){
+            do {
+
+                price = cursor.getString(cursor.getColumnIndex(COLUMN_DATE)).toString()
+                list.add(price)
+                //Toast.makeText(Context, name, Toast.LENGTH_LONG).show()
+
+            }while (cursor.moveToNext())
+        }
+
+        return list
+    }
+
+
+
+
+
+    fun getReceiptDate(id: Int): String{
+
+        val db = this.readableDatabase
+        val query= "SELECT "+ COLUMN_DATE + " FROM " + RECEIPT + " WHERE " + COLUMN_RECEIPT_ID + " = " + id + " ORDER BY " +
+                COLUMN_DATE + " LIMIT 1"
+
+        var cursor = db.rawQuery(query, null)
+
+        if(cursor.moveToNext()){
+            // Toast.makeText(Context, cursor.getString(cursor.getColumnIndex(COLUMN_ITEM_ID)), Toast.LENGTH_LONG).show()
+        }
+        var date = cursor.getString(cursor.getColumnIndex(COLUMN_DATE)).toString()
+
+        return date
+
+
+
+    }
+
+    fun getReceiptTotal_Price(id: Int): String{
+
+        val db = this.readableDatabase
+        val query= "SELECT "+ COLUMN_PRICE + " FROM " + RECEIPT + " WHERE " + COLUMN_RECEIPT_ID + " = " + id + " ORDER BY " +
+                COLUMN_PRICE + " LIMIT 1"
+
+        var cursor = db.rawQuery(query, null)
+
+        if(cursor.moveToNext()){
+            // Toast.makeText(Context, cursor.getString(cursor.getColumnIndex(COLUMN_ITEM_ID)), Toast.LENGTH_LONG).show()
+        }
+        var price = cursor.getString(cursor.getColumnIndex(COLUMN_PRICE)).toString()
+
+        return price
+    }
+
+
+
+
+
+
+
+
+
+
+    fun getLastReceiptID():Int{
+
+        val db = this.readableDatabase
+        val query= "SELECT " + COLUMN_RECEIPT_ID + " FROM " + RECEIPT + " ORDER BY " + COLUMN_RECEIPT_ID + " DESC LIMIT 1"
+
+        var cursor = db.rawQuery(query, null)
+
+        if(cursor.moveToNext()){
+            // Toast.makeText(Context, cursor.getString(cursor.getColumnIndex(COLUMN_ITEM_ID)), Toast.LENGTH_LONG).show()
+        }
+        var id = cursor.getString(cursor.getColumnIndex(COLUMN_RECEIPT_ID)).toInt()
+        cursor.close()
+        return id
     }
 
 
@@ -468,16 +714,38 @@ class DatabaseHelper(var Context: Context):
     /* FUNCTION OF DELETING IN THE DATABASE */
     /****************************************/
 
-    fun updateItem(itemName: String, price:Int, category: String){
+    // This function is used to update an item in the database
+    fun updateItem(itemName: String, price:Int, category: String, Itemid: Int){
 
         val db = this.writableDatabase
         val cv = ContentValues()
         cv.put(COLUMN_ITEM_NAME, itemName)
         cv.put(COLUMN_PRICE, price)
 
-       // database.update(TABLE_NAME, cv, ""+KEY_UserName+"= '"+ username+"' AND "+KEY_CID+"='"+Cid+"'  AND "+KEY_Password+"='"+password+"'" , null);
-        //        database.close();
+        //get the current category of the item
+        val currentCategory = getItemCategoryInBelong(Itemid)
+        val futureCategory= getCategoryID(category)
 
+        //update the table item and close cursor
+        val query= "UPDATE "+ ITEM + " SET "+ COLUMN_ITEM_NAME + " = '"+itemName+"' " +
+                ", "+ COLUMN_PRICE + " = "+ price + " WHERE " + COLUMN_ITEM_ID + " = " + Itemid
+
+        var cursor = db.rawQuery(query, null)
+        cursor.moveToFirst();
+        cursor.close();
+
+        //Check if category has changed, if yes, then update
+        if (!currentCategory.equals(futureCategory))
+        {
+            Toast.makeText(Context, "different category to update", Toast.LENGTH_LONG).show()
+
+            val queryTemp= "UPDATE "+ BELONG + " SET "+ COLUMN_BELONG_CATEGORY_ID + " = " + futureCategory +
+                    " WHERE " + COLUMN_BELONG_ITEM_ID + " = " + Itemid
+
+            val cursorTemp = db.rawQuery(queryTemp, null)
+            cursorTemp.moveToFirst()
+            cursor.close()
+        }
     }
 
  }
