897466d19e497309983c2722c02da887cd69d22c
arielmukadi <45185718+arielmukadi@users.noreply.github.com>
2021-05-29 15:30:34 -0700

    Channel Notification implimentation

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,95 +1,74 @@
-package com.example.mycameraapplication
-import android.net.Uri
+package com.example.notificationapp
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.karumi.dexter.Dexter
+import android.widget.Button
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
-import java.util.jar.Manifest
-
-import android.app.Activity
-import android.content.ContentValues
-import android.content.Intent
-import android.graphics.Bitmap
-import android.provider.MediaStore
-import android.widget.Toast
-import com.karumi.dexter.MultiplePermissionsReport
-import com.karumi.dexter.PermissionToken
-import com.karumi.dexter.listener.PermissionRequest
-import com.karumi.dexter.listener.multi.MultiplePermissionsListener
-//import kotlinx.android.synthetic.main.activity_main.*
-import java.lang.Exception
-
-
-//import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    companion object{
-        val MY_CAMERA_REQUEST_CODE = 8484  // any number you want
-    }
-
-    var imageUri:Uri= null!!
+    private val CHANNEL_ID ="channel_id_exemple_01"
+    private val notificationId = 101
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Event
-        btn_camera.setOnClickListener{
-            Dexter.withContext(this)
-                .withPermissions(Arrays.asList(
-
-                     android.Manifest.permission.READ_EXTERNAL_STORAGE,
-                     android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                      android.Manifest.permission.CAMERA
+        createNotificationChannel()
+        btn_button.setOnClickListener {
+            sendNotification()
+        }
 
 
 
 
-                )).withListener(object:MultiplePermissionsListener{
-                        override fun onPermissionsChecked(p0: MultiplePermissionsReport?) {
-                            if (p0!!.areAllPermissionsGranted()) {
-                                val values = ContentValues()
-                                values.put(MediaStore.Images.Media.TITLE, "NEW PICTURE")
-                                values.put(MediaStore.Images.Media.DESCRIPTION, "From yor Camera")
-                                imageUri= contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)!!
-                                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
-                                startActivityForResult(intent, MY_CAMERA_REQUEST_CODE)
-
-                            }
-                            else{
-                                Toast.makeText(this@MainActivity, "You must accept all the permissin", Toast.LENGTH_SHORT).show()
-                            }
-                        }
-
-                        override fun onPermissionRationaleShouldBeShown(p0: MutableList<PermissionRequest>?, p1: PermissionToken?) {
-                            TODO("Not yet implemented")
-                        }
-                    })
-
-
-
-
-
-
-
-       }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == MY_CAMERA_REQUEST_CODE){
-            if(resultCode == RESULT_OK){
-                try{
-                    val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, imageUri!!)
-                    imageView.setImageBitmap(bitmap)
-                }catch(e:Exception){
-                    e.printStackTrace()
-                }
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+
+            val name = "Notification Title"
+            val descriptionText ="Notification Description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID,name,importance).apply {
+                description=descriptionText
             }
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+
+
         }
     }
+    private fun sendNotification() {
+        val intent = Intent(this,MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
 
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this,0,intent,0)
+
+        val bitmap = BitmapFactory.decodeResource(applicationContext.resources, R.drawable.android)
+        val bitmapLargeIcon = BitmapFactory.decodeResource(applicationContext.resources, R.drawable.steam)
+
+        val builder = NotificationCompat.Builder(this,CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("notification ariel")
+            .setContentText("Nezy Mutu monene")
+            .setLargeIcon(bitmapLargeIcon)
+           .setStyle(NotificationCompat.BigTextStyle().bigText("Much longer text cannot display please click on it nezy"))
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        with(NotificationManagerCompat.from(this)) {
+            notify(notificationId,builder.build())
+
+        }
+    }
 }
